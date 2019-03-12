c16db09e8484261fb0980f5b1aeafb1daafcbb57
Tilanie <44407626+Tilanie@users.noreply.github.com>
2019-03-12 15:14:32 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
-
 var express = require("express");
-var bodyParser = require("body-parser"); lines (6 sloc)  164 Bytes
-
+var bodyParser = require("body-parser");
+const https = require('https')
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,12 +9,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var PORT = 3000;
 
 app.post('/authenticate',function(request,response){
-	var val1 = request.body.var1;
-	var val1 = request.body.var1;
+	var val1 = request.body.type;
+	var val2 = request.body.data;
 	console.log("Authenticate on post");
 });
 
+app.get('/authenticate',function(request,response){
+	var val1 = request.body.type;
+	var val2 = request.body.data;
+	console.log("Authenticate on get");
+});
 
 app.listen(PORT);
 console.log("Server is running on "+ PORT +" port");
 
+
+
+
+const data = JSON.stringify({
+  todo: 'Buy the milk'
+})
+
+const options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
